4055423b122858225b7554b8e399f5814fed22fa
Robert <31496215+TianMaXingKong2003@users.noreply.github.com>
2017-10-16 20:55:18 +0800

    Update ZJStrategy2017.h

--- ZJStrategy2017.h
+++ ZJStrategy2017.h
@@ -1,1 +1,28 @@
 
+//	ZJStrategy2017.h	由于平台的头文件经过多届学长修改，建议学弟们在此基础上试着重写，便于熟悉变量与常数的定义 	 
+
+#ifndef
+
+	#define 
+
+#else
+
+	#define
+
+#endif
+
+
+//----------------------
+
+#include<stdio.h>
+#include<math.h>
+#include<windows.h>
+#include<fstream>
+
+//----------------------
+
+//球员个数
+const long PLAYERS_PER_SIDE=5；
+const long PLACE_KICK=2;
+const long  
+ 
